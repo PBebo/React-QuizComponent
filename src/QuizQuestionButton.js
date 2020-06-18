d8e56f35ae5f5df867e467885cd2d6e2bd4cd9fb
Pierrick BOBET <pierrick.bobet@gmail.com>
2020-06-18 11:09:57 +0200

    Step ok

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 class QuizQuestionButton extends React.Component {
-  render(){
+  render() {
     return (
       <li>
         <button>{this.props.button_text}</button>
